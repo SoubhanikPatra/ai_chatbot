83a5d767d3e29cbb934227840caf16242df6ba37
SoubhanikPatra <soubhanikpatra@gmail.com>
2025-03-01 19:15:11 +0530

    removed unnecessary lines

--- gemini_utility.py
+++ gemini_utility.py
@@ -1,6 +1,5 @@
 import os
 
-import json
 import google.generativeai as genai
 from dotenv import load_dotenv
 
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-
 import streamlit as st
 from streamlit_option_menu import option_menu
 
